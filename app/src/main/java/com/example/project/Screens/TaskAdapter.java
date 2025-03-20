004459df74719ab9553d33cd50e415a83a488518
123shalev <shalevfir25@gmail.com>
2025-03-20 11:50:54 +0200

    end 20.3

--- app/src/main/java/com/example/project/Screens/TaskAdapter.java
+++ app/src/main/java/com/example/project/Screens/TaskAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,11 +18,17 @@ import java.util.List;
 public class TaskAdapter extends ArrayAdapter<Task> {
     private Context context;
     private List<Task> taskList;
+    private OnTaskDeleteListener deleteListener; // ממשק למחיקה
+
+    public interface OnTaskDeleteListener {
+        void onTaskDelete(int position);
+    }
 
     public TaskAdapter(Context context, List<Task> taskList) {
         super(context, R.layout.task_item, taskList);
         this.context = context;
         this.taskList = taskList;
+        this.deleteListener = deleteListener; // שמירת הליסינר
     }
 
     @NonNull
@@ -35,11 +42,19 @@ public class TaskAdapter extends ArrayAdapter<Task> {
 
         TextView tvTaskName = convertView.findViewById(R.id.tvTaskName);
         TextView tvDeadline = convertView.findViewById(R.id.tvDeadline);
+        ImageButton btnDeleteTask = convertView.findViewById(R.id.btnDeleteTask);
 
         if (task != null) {
             tvTaskName.setText(task.getName());
             tvDeadline.setText(task.getDeadlineDate() + " - " + task.getDeadlineTime());
         }
+
+        // הוספת אירוע למחיקת משימה
+        btnDeleteTask.setOnClickListener(v -> {
+            if (deleteListener != null) {
+                deleteListener.onTaskDelete(position);
+            }
+        });
 
         return convertView;
     }
